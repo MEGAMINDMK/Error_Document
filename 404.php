c67ca07451bbc00e2ddb7d067a33c0a6b5b4f65c
webnet <muhommadraahim1@gmail.com>
2018-10-16 02:29:34 -0700

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,30 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="icon" href="/404img/bn.png" type="image/png" sizes="16x16">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.page-wrap {
+    min-height: 100vh;
+}
+body {
+   background-image: url("/404img/Blue-galaxy.jpg");
+   background-color: black;
+    height: 100%;
+	position: relative;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block"><font color="white"><b>404</b></font></span>
+                <div class="mb-4 lead"><font color="white">The page you are looking for was not found.</font></div>
+                <a href="http://wno-host.ddns.net:90" class="btn btn-link"><font color="white"><b>Back to Home</b></font></a>
+            </div>
+        </div>
+    </div>
+</div>
